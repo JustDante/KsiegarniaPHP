fb15470601fa14e225fd6425c073176b2ae95d1c
JKida10 <49513156+JKida10@users.noreply.github.com>
2019-12-28 17:58:51 +0100

    index(bez szczegółów książki)

---
+++ index.php
@@ -0,0 +1,49 @@
+<! DOCTYPE html>
+<html>
+    <head>
+        <title>Księgarnia</title>
+        <link rel="stylesheet" type="text/css" href="style/style.css" />
+    </head>
+    <body>
+        <div id="container">
+
+            <div id="header">
+                <h1>Księgarnia - Kup Sobie Kniszke !<h1>
+            </div>
+
+            <div id="content">
+
+                <div id="category">
+                <h2>Kategorie</h2>
+                    <ul>
+                        <li><a href="category_page.php"> Fantasy </a></li>
+                        <li><a href="category_page.php"> Kucharskie </a></li>
+                        <li><a href="category_page.php"> Kryminał </a></li>
+                </div>
+
+                <div id="list">
+                <h2>Lista Książek</h2> 
+
+                    <div id="grid_view">
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                        <div id="product"> <img src="image.jpg" width="50" /><h3>Kniszka</h3></div>
+                    </div>
+
+                </div>
+            </div>
+            <div id="footer">
+                Copyright &copy; 2019 Materna Kida
+            </div>
+       </div>
+    </body>
+</html>
